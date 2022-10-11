6f78b3636db4873ff9e3bc47d28490995e71ef80
Myitschool <galyadavydenko@yandex.ru>
2022-10-11 19:04:26 +0700

    new

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,32 @@ public class Main {
         }
     }
 
+    public static void InputMass(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+
+
+                a[i][j] = 10 + (int) (Math.random() * (100 - 10 + 1));
+            }
+        }
+    }
+
     public static void OutputMass(int[] a) {
         for (int x : a) {
             System.out.print(x + " ");
         }
         System.out.println();
+    }
+
+    public static void OutputMass(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+
+            for(int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     public static int[] MinMax(int[] a) {
@@ -71,19 +92,46 @@ public class Main {
 
     }
 
+    static void pringG()
+    {
+        System.out.println("G= " + G);
+    }
+
+    static final double G = 6.6720e-10;
+    static int x;
+    static int y;
+    static {
+        x = 5;
+        y = 100;
+    }
+    static int Summ(int a, int b)
+    {
+        return x + y;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+
+        pringG();
+        System.out.println();
+        double PI = Math.PI;
+        int z = Summ(1, 1);
+        System.out.println(z);
+
+        /*int n = in.nextInt();
         int[] a = new int[n];
         InputMass(a);
-        OutputMass(a);
+        OutputMass(a);*/
 
         // Reverse(a);
      //   Sdvig_LK(a, 4);
-        rotRight(a);
+        /*rotRight(a);
 
-        OutputMass(a);
+        OutputMass(a);*/
 //        InputMass(b);
+        int [][] matrix = new int[10][10];
+        InputMass(matrix);
+        OutputMass(matrix);
 
 
     }
